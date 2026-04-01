58c74f4b9cf71b8c2560cbebf49157c647fbab41
Abhinav-FP <a.mathur@internetbusinesssolutionsindia.com>
2026-04-01 12:44:50 +0530

    Updated prisma version

--- src/prismaconfig.js
+++ src/prismaconfig.js
@@ -1,12 +1,20 @@
-// prisma.js or prismaconfig.js
 const { PrismaClient } = require('@prisma/client');
+const { Pool } = require('pg');
+const { PrismaPg } = require('@prisma/adapter-pg');
+
+// Create PostgreSQL pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// Create Prisma adapter
+const adapter = new PrismaPg(pool);
 
 let prisma;
 
 if (!global._prisma) {
-  global._prisma = new PrismaClient();
+    global._prisma = new PrismaClient({adapter});
 }
 
 prisma = global._prisma;
-
 module.exports = prisma;
